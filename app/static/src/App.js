ec9950baeda7023320817ef35b2bbbd9d33e1183
Omar Nasr <omardev9898@gmail.com>
2019-04-08 22:07:13 -0400

    decided to go with react

---
+++ app/static/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+function App(){
+    return (
+        <div>
+            <Header/>
+            <Main/>
+        </div>
+    )
+}
+
+export default App
---
+++ app/static/src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Main(){
+    return (
+        <main>
+            <input type = "checkbox">
+            </input>
+            <p>Learn react</p>
+        </main>
+    )
+}
+
+export default Main

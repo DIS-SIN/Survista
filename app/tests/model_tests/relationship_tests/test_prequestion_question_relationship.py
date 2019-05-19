b26951e62da78286eb20059483c8eaf7ca69577f
Omar Nasr <omardev9898@gmail.com>
2019-05-19 18:25:30 -0400

    Test Question -> Prequestion relationship

---
+++ app/tests/model_tests/relationship_tests/test_prequestion_question_relationship.py
@@ -0,0 +1,39 @@
+import pytest
+from src import create_app
+from datetime import datetime
+
+class Test_Prequestion_Question_Relationship:
+
+    app = create_app(
+        mode="development",
+        static_path="../static",
+        templates_path="../templates",
+        instance_path="../instance"
+    )
+
+    def test_create_relationship(self):
+        with self.app.app_context():
+            from src.database.db import init_db, distroy_db, get_db
+            from src.models.question_model import Question, PreQuestion
+
+            distroy_db(self.app)
+            init_db(self.app)
+
+            current_transaction = get_db().transaction
+
+            with current_transaction:
+                test_question_1 = Question(
+                    question="Test Question 1",
+                    slug="test_question_1",
+                    language="en"
+                )
+                test_question_1.save()
+                test_prequestion_1 = PreQuestion(
+                    text = "This is an example prequestion",
+                    slug = "test_prequestion_1",
+                    language = "en"
+                )
+                test_prequestion_1.save()
+
+                test_prequestion_1.questions.connect(test_question_1)
+                
